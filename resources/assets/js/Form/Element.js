8ce4f5759eca63632a69330416796335e82ce62b
Fabricio Oliveira <fabricio.jhonata@gmail.com>
2018-03-20 23:13:24 -0300

    inicializando projeto

---
+++ resources/assets/js/Form/Element.js
@@ -0,0 +1,9 @@
+import $ from 'jquery'
+
+export default class Element{
+  constructor(element){
+    this.name = $(element).attr("name");
+    this.pretty_name = $(element).attr("data-ftb-input-pretty-name");
+    this.element = element;
+  }
+}
---
+++ resources/assets/js/utils.js
@@ -0,0 +1,7 @@
+export const Lucas = (pegaria) => {
+  if(pegaria == "bocao"){
+    console.log("Claaaaro!")
+  }else{
+    console.log("Eu namoro!")
+  }
+}
